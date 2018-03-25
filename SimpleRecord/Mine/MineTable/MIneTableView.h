55f4e33f9af639fff982060e6f539de4d43ae47e
bumingxialuo <690148814@qq.com>
2018-03-25 09:42:05 +0800

    试试自己的电脑能不能上传

---
+++ SimpleRecord/Mine/MineTable/MIneTableView.h
@@ -0,0 +1,13 @@
+//
+//  MIneTableView.h
+//  SimpleRecord
+//
+//  Created by 不明下落 on 2018/3/25.
+//  Copyright © 2018年 xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MIneTableView : UITableView
+
+@end
---
+++ SimpleRecord/Mine/MineTable/MIneTableView.m
@@ -0,0 +1,21 @@
+//
+//  MIneTableView.m
+//  SimpleRecord
+//
+//  Created by 不明下落 on 2018/3/25.
+//  Copyright © 2018年 xia. All rights reserved.
+//
+
+#import "MIneTableView.h"
+
+@implementation MIneTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
